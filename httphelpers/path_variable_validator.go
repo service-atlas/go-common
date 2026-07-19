6d3a03f3658d6c49913ef8023c689eeef0e2ef87
Josh Potts <9337140+jlpdeveloper@users.noreply.github.com>
2026-07-19 12:00:22 -0500

    Add PathVariableValidator utility with support for GUID, date, and integer validation, including tests

---
+++ httphelpers/path_variable_validator.go
@@ -0,0 +1,57 @@
+package internal
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+var pathValueLookup func(*http.Request, string) string
+
+// SetPathValueLookup sets a custom function to retrieve values from request paths based on a key.
+func SetPathValueLookup(fn func(*http.Request, string) string) {
+	pathValueLookup = fn
+}
+
+type PathValidator func(string, *http.Request) (string, bool)
+
+func getPathValue(req *http.Request, varName string) string {
+	val := req.PathValue(varName)
+	if val != "" {
+		return val
+	}
+	if pathValueLookup != nil {
+		return pathValueLookup(req, varName)
+	}
+	return ""
+}
+
+func GetGuidFromRequestPath(varName string, req *http.Request) (string, bool) {
+	guidVal := getPathValue(req, varName)
+	return IsValidGuid(guidVal)
+}
+
+func IsValidGuid(guidVal string) (string, bool) {
+	err := uuid.Validate(guidVal)
+	return guidVal, err == nil
+}
+
+func GetDateFromRequestPath(varName string, req *http.Request) (time.Time, bool) {
+	dateVal := getPathValue(req, varName)
+	date, err := time.Parse("2006-01-02", dateVal)
+	return date, err == nil
+}
+
+func GetIntFromRequestPath(varName string, req *http.Request) (int, bool) {
+	val := getPathValue(req, varName)
+	if val == "" {
+		return 0, false
+	}
+	id, err := strconv.Atoi(val)
+	if err != nil || id <= 0 {
+		return 0, false
+	}
+	return id, true
+}
